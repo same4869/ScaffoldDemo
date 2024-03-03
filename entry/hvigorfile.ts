e39477356677f65911193ea127da1b7070b2f8fa
xwang <same4869@126.com>
2024-03-03 19:43:47 +0800

    update

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,2 +1,22 @@
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
 export { hapTasks } from '@ohos/hvigor-ohos-plugin';
+
+// function helloWorld() {
+//   return {
+//     pluginId: 'HelloWorld',
+//     apply(pluginContext) {
+//       pluginContext.registerTask({
+//         // 编写自定义任务
+//         name: 'HelloWorld',
+//         run: (taskContext) => {
+//           console.log('Hello World!');
+//         }
+//       })
+//     }
+//   }
+// }
+//
+// export default {
+//   system: [],
+//   plugins: [ helloWorld() ]
+// }
